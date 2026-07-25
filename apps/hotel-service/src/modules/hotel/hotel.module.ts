bb4c0b4a4305e6e9f3fa44b9771401d7704ed2f4
ImagINN <muhammed.gokhan55@gmail.com>
2026-07-26 02:04:09 +0300

    feat: implement hotel and restaurant services with gRPC support and add trip planning controller

---
+++ apps/hotel-service/src/modules/hotel/hotel.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HotelEntity } from './entities/hotel.entity';
+import { HotelController } from './hotel.controller';
+import { HotelService } from './hotel.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([HotelEntity])],
+  controllers: [HotelController],
+  providers: [HotelService],
+})
+export class HotelModule {}
